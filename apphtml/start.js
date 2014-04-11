f6d3ae7c4a7721c858f98114da669e5a138d3e05
Tom Clarkson <tom@orangeguava.com>
2014-04-11 15:12:53 +1000

    swipe

--- apphtml/start.js
+++ apphtml/start.js
@@ -1,9 +1,12 @@
 app.createViewTemplate({
     templateId: "start",
-    
+
     route: "/start",
     controller: function($scope) {
         $scope.exampleValue = "Example";
+
+        $scope.leftRoute = "/modalpage1";
+        $scope.rightRoute = "/modalpage2";
 
         $scope.exampleFunction = function() {
             $scope.exampleValue = '';
